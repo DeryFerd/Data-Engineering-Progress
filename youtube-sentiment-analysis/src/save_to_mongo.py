b968ff32abcd584547c6610524f3ec3ab0199de1
Dery <114888155+DeryFerd@users.noreply.github.com>
2025-07-17 14:45:54 +0700

    Update save_to_mongo.py

--- youtube-sentiment-analysis/src/save_to_mongo.py
+++ youtube-sentiment-analysis/src/save_to_mongo.py
@@ -3,22 +3,21 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 from fetch_comments import get_comments
 
-# Load .env untuk YOUTUBE_API_KEY (walau gak dipakai langsung, biar konsisten)
 load_dotenv()
 
-# Koneksi ke MongoDB lokal
+# Connect to MongoDB local
 client = MongoClient("mongodb://localhost:27017/")
 db = client["youtube_db"]
 collection = db["comments"]
 
-# Ambil komentar dari video
+# Scraping comments from Youtube video
 video_id = "CRraHg4Ks_g"
 comments = get_comments(video_id, max_results=500)
 
-# Format dan simpan ke MongoDB
+# Save comments to MongoDB
 data_to_insert = [{"video_id": video_id, "comment": c} for c in comments]
 if data_to_insert:
     collection.insert_many(data_to_insert)
-    print(f"{len(data_to_insert)} komentar berhasil disimpan ke MongoDB.")
+    print(f"{len(data_to_insert)} comments sucessfully saved to MongoDB.")
 else:
-    print("Tidak ada komentar yang disimpan.")
+    print("No comments are saved.")
