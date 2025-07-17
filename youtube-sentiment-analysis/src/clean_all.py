546a8eb6ec94d61b24277918ad9e49ad306b58c8
Dery <114888155+DeryFerd@users.noreply.github.com>
2025-07-17 14:40:14 +0700

    Update clean_all.py

--- youtube-sentiment-analysis/src/clean_all.py
+++ youtube-sentiment-analysis/src/clean_all.py
@@ -2,20 +2,20 @@ from pymongo import MongoClient
 import pandas as pd
 from preprocess import clean_text
 
-# Koneksi ke MongoDB
+# Connect to MongoDB
 client = MongoClient("mongodb://localhost:27017/")
 db = client["youtube_db"]
 collection = db["comments"]
 
-# Ambil data dari MongoDB
+# Load data from MongoDB
 data = list(collection.find())
 df = pd.DataFrame(data)
 
-# Bersihkan semua komentar
+# Clean all comments
 df["cleaned_comment"] = df["comment"].apply(clean_text)
 
-# Tampilkan 10 komentar pertama
+# Show top 10 comments
 print(df[["comment", "cleaned_comment"]].head(10))
 
-# Simpan hasil ke CSV (opsional)
+# Save to CSV
 df.to_csv("data/cleaned_comments.csv", index=False, encoding='utf-8-sig')
